73358aa49f794fab6caf709fd87f820c7f767f4d
JiayangYing <113895670+JiayangYing@users.noreply.github.com>
2025-04-29 15:37:28 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://JiayangYing.vercel.app/",
-	base: "jiayang",
+	base: "JiayangYing",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
