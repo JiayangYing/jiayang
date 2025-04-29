1b1efe2992ab3e816e01b98d2a485322d978e684
JiayangYing <113895670+JiayangYing@users.noreply.github.com>
2025-04-29 15:33:48 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,10 +17,9 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
-import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jiayang.vercel.app/",
+	site: "https://JiayangYing.vercel.app/",
 	base: "jiayang",
 	trailingSlash: "always",
 	integrations: [
