0a367bcb8060bb79f98365f35628f184c18b7fbc
JiayangYing <113895670+JiayangYing@users.noreply.github.com>
2025-04-29 16:01:33 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Jiyang Ying",
+	name: "Jiayang Ying",
 	bio: "This blog is where I track my progress.",
 	links: [
 		{
